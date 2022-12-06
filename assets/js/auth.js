3db7d207907b804ba0fb3afd30d983c95a222ac7
johnkul001 <johnkul001@gmail.com>
2022-12-06 21:55:40 +0000

    sign up and login pages added

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -4,15 +4,6 @@ import { createClient } from '@supabase/supabase-js'
 (function () {
     "use strict";
 
-    // fetch('https://www.themuse.com/api/public/jobs?page=1&descending=true', {
-    //     method: 'GET',
-    //     headers: {
-    //         'Accept': 'application/json',
-    //     },
-    // })
-    //     .then(response => response.text())
-    //     .then(text => console.log(text))
-
     // Create a single supabase client for interacting with your database
 
     const options = {
@@ -22,19 +13,46 @@ import { createClient } from '@supabase/supabase-js'
     }
 
     const supabase = createClient("https://xyzcompany.supabase.co", "public-anon-key", options)
-    
+
     const signIn = async (email, password) => {
         const { data, error } = await supabase.auth.signInWithPassword({
-            email: email,        
-            password: password,        
-          })
+            email: email,
+            password: password,
+        })
     }
 
     const signUp = async (email, password) => {
         const { data, error } = await supabase.auth.signUp({
             email: email,
-            password: password 
+            password: password
         })
+
+        const result = await supabase.from('users').insert(data)
+
+        console.log(response)
+    }
+
+    function getData(form) {
+        var formData = new FormData(form);
+        return Object.fromEntries(formData)
+    }
+
+    const signInForm = document.getElementById("signInForm")
+
+    if (typeof signInForm !== 'undefined' && signInForm !== null) {
+        signInForm.onsubmit = function (e) {
+            e.preventDefault()
+            console.log(getData(e.target))
+        }        
     }
     
+    const signUpForm = document.getElementById("signUpForm")
+
+    if  ( typeof signInForm !== 'undefined' && signUpForm !== null) {
+        document.getElementById("signUpForm").onsubmit = function (e) {
+            e.preventDefault()
+            console.log(getData(e.target))
+        }   
+    }
+
 })()
